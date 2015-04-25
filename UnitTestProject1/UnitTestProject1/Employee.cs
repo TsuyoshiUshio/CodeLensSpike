2cd0e57b6ffe3928fec104d4c6d027b5eec69aaa
Tsuyoshi Ushio <ushio@simplearchitect.com>
2015-04-25 14:22:52 +0900

    Add parameter

---
+++ UnitTestProject1/UnitTestProject1/Employee.cs
@@ -0,0 +1,19 @@
+﻿namespace UnitTestProject1
+{
+    public class Employee
+    {
+        public Employee(int id, string name, int baseSalary)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.BaseSalary = baseSalary;
+
+        }
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int BaseSalary { get; set; }
+    }
+}
---
+++ UnitTestProject1/UnitTestProject1/EmployeeTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class EmployeeTest
+    {
+        [TestMethod]
+        public void TestInitialize()
+        {
+            Employee employee = new Employee(1, "山田", 100);
+            Assert.AreEqual(1, employee.Id);
+            Assert.AreEqual("山田", employee.Name);
+            Assert.AreEqual(100, employee.BaseSalary);
+        }
+    }
+}
